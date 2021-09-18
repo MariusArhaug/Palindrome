5284d08bd3354f64fc01aca4c5a49a314616260d
MariusArhaug <mariusarhaug@hotmail.com>
2021-09-18 02:22:14 +0200

    FEAT: final version complete with LED lightup and JTAG print

--- palindrome.s
+++ palindrome.s
@@ -3,116 +3,115 @@
 .section .text
 
 _start:
-	mov r0, #0 							// r0 = i = 0
-	ldr r1, =input					// input words
-	mov r2, #0							// r2 = j = 0, but we need it to be length of word - 1 				
-	
-	ldrb r3, [r1, r2] 			// first char 
-	
-	b find_length
-	sub r2, r2, #-1 				// j = len(string) - 1
-	
-	b check_palindrome
-	b palindrome_found	
-	
-	
-find_length: 							// find and set length of input string 
-	cmp r3, #0							// copare current char with end of string
-	bne check_input_main		// if not end of string iterate 
+	LDR r0, =input						// input words
+	MOV r1, #0								// r0 = i = 0 				
+	MOV r2, #0 								// r2 = j = 0
+	LDR r5, =0xFF200000				// address of LEDs
+	STR r1, [r5]							// reset LEDs
 
-	mov pc, lr							// After loop move pc back to start function call 	
-	
-	find_length_main:
-	add r2, r2, #1 					// Increment r2 (j++)
-	ldrb r3 [r1, r2]				// update r3 to be next char, string[j]
-	b find_length 					// branch to loop
+	B find_length							// update j value to be length of word
 	
 	
-check_palindrome: 				// Main function in program
-	cmp r0, r2							// while r0 < r2 do while_palindrome
-	blt while_palindrome
-	mov pc, lr							// Move pc back to _start and branch for palindrome found since it did not break out
-	while_palindrome:				// while r0 < r2  
-
-		cmp r3, #0x20					// if string[i] == ' ' => i++
-		beq increment_i		
-		increment_i:
-			add r0, r0, #1
-
-		cmp r4, 0x20				// if string[j] == ' ' => j--
-		beq decrement_j
-		decrement_j:
-			sub r1, r1, #-1
-
-		b compare_chars
-		add r0, r0, #1
-		sub r1, r1, #-1
-		b check_palindrome
-	
+find_length: 								// find and set length of input string
+	LDRB r3, [r0, r2]					// r3 = string[i]
+	CMP r3, #0								// check if string[i] is at end of string
+	BEQ main									// if end of string go to main 
+	ADD r2, r2, #1 						// Increment r2 (j++)
+	B find_length 						// iterate again
 
 
-compare_chars:
-	cmp r3, #0x41				// if hex value of string[i] is greater than 41 branch to capital checker 
-	bgt is_capital_i
-	
-	cmp r4, #0x41
-	bgt is_capital_j
-
-	ldrb r3, [r1, r0]		// r3 = string[i]
-	ldrb r4, [r1, r2]		// r4 = string[j]
-
-	cmp r3, r4					//if string[i] != string[j] => palindrome not found
-	bne palindrom_not_found 
-		
-
-	
-is_capital_i:					// if current char is capital |  0x41 < string[i] < 0x5A
-	cmp r3, #0x5A						
-	blt is_capital_main_i
-	
-	is_capital_main_i:
-	add r3, r3, #0x20			// convert capital letter to non capital letter
-		
-
-is_capital_j:
-	cmp r4, 0x5A
-	blt is_capital_main_j
-	
-	is_capital_main_i:
-	add r4, r4, #0x20			// convert capital letter to non capital letter
+main: 
+	SUB r2, r2, #1	 					// j = len(string) - 1
+	B check_palindrome
 
 
+check_palindrome: 					
+	CMP r1, r2								// while r1 < r2 (i < j) do while_palindrome
+	BLT while_palindrome
+	B palindrome_found
 
-palindrome_found:
-	// Switch on only the 5 rightmost LEDs
-	// Write 'Palindrome detected' to UART
+while_palindrome:						// while r0 < r2  
+	LDRB r3, [r0, r1]					// r3 = string[i]
+	LDRB r4, [r0, r2]					// r4 = string[j]
+	CMP r3, #0x20							// if string[i] is space
+	ADDEQ r1, r1, #1					// i++
+
+	CMP r4, #0x20							// if string[j] is space
+	SUBEQ r2, r2, #1					// j--
+
+	LDRB r3, [r0, r1]					// r3 = string[i]
+	LDRB r4, [r0, r2]					// r4 = string[j]
+
+	CMP r3, #0x40							// if (0x40 < r3 < 0x5B) then r3 is capital 
+	CMPGT r3, #0x5B					
+	ADDLT r3, r3, #0x20				// make r3 non capital
+
+	CMP r4, #0x40							// if (0x40 < r4 < 0x5B) then r4 is capital
+	CMPGT r4, #0x5B			
+	ADDLT r4, r4, #0x20				// make r4 non capital
+
+
+	CMP r3, r4								//if string[i] != string[j] => palindrome not found
+	BNE palindrom_not_found 	// exit while loop
+
+	ADD r1, r1, #1						// i++
+	SUB r2, r2, #1						// j--
+	B check_palindrome				// go to iterate checker
 	
-	b exit
+
+
+palindrome_found:						// Switch on only the 5 rightmost LEDs. Write 'Palindrome detected' to UART
+	LDR r6, =success
+	LDR r8, =0xFE0
 	
-palindrom_not_found:
-	// Switch on only the 5 leftmost LEDs
-	// Write 'Not a palindrome' to UART
-	b exit
+	B print_message
 	
-exit:
-	// Branch here for exit
-	b exit
+palindrom_not_found: 				//Switch on only the 5 leftmost LEDs. Write 'Not a palindrome' to UART
+	LDR r6, =error
+	LDR r8, =0x1F
+	B print_message
+
+print_message:
+	LDR r5, =0xFF201000 		// load JTAG address
+	MOV r7, #0							// i = 0
+	print_loop:
+		LDRB r2, [r6, r7] 		// string[i]
+		CMP r2, #0		  			// if string is at end of line, exit
+		BEQ light_up	  			// When done printing, light up
+		STR r2, [r5]					// store string[i] in r5
+		ADD r7, r7, #1	  		// i++
+		B print_loop
+
+light_up:
+	LDR r5, =0xFF200000			// load LED address
+	STR r8, [r5]
+	B exit
+	
+	
+exit:	// exit program
+	B exit	
 	
 
 .section .data
 .align
-	// This is the input you are supposed to check for a palindrome
-	// You can modify the string during development, however you
-	// are not allowed to change the label 'input'!
-	input: .asciz "level"
- 
-	light_address: .word 0xff200000
-	
-	// input: .asciz "8448"
-    // input: .asciz "KayAk"
-    // input: .asciz "step on no pets"
-    // input: .asciz "Never odd or even"
 
+	
+ 	error: .asciz "Not a palindrome \n"
+	success: .asciz "Palindrome detected \n"
+	
+	// Test Palindromes: 
+	input: .asciz "level"				// ok!			
+	// input: .asciz "LEVEL"				// ok!
+	// input: .asciz "LEvel" 				// ok! 							
+	// input: .asciz "8448"					// ok!					
+    // input: .asciz "KayAk"				// ok!				
+    // input: .asciz "step on no pets"		// ok!
+    // input: .asciz "Never odd or even"	// ok!	
+	
+	// Test non palindromes: 
+	//input: .asciz "Palindrome"			// ok!
+	//input: .asciz "84418"					// ok!
+	//input: .asciz "First level"			// ok!
 
 .end
 
